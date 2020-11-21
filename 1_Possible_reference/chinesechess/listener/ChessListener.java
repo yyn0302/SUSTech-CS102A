eec99161b8ae45b12f7f1df671b04b855d8d8e47
Chris <hezean@vip.qq.com>
2020-11-21 17:51:38 +0800

    Amend

--- 1_Possible_reference/chinesechess/listener/ChessListener.java
+++
@@ -1,9 +0,0 @@
-package xyz.chengzi.cs102a.chinesechess.listener;
-
-import xyz.chengzi.cs102a.chinesechess.chess.ChessComponent;
-
-public abstract class ChessListener {
-    public void onClick(ChessComponent chessComponent) {
-        // Do absolutely nothing.
-    }
-}
--- Proj/src/df.java
+++
@@ -1,5 +0,0 @@
-public class df {
-    public static void main(String[] args) {
-        
-    }
-}
