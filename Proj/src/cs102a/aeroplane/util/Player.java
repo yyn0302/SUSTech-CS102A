94c25e622254c283395db479296557f7fe84a84e
Tonystarkw12 <72902109+Tonystarkw12@users.noreply.github.com>
2020-12-08 21:54:55 +0800

    改好了

--- Proj/src/cs102a/aeroplane/util/Player.java
+++ Proj/src/cs102a/aeroplane/util/Player.java
@@ -1,13 +1,21 @@
 package cs102a.aeroplane.util;
 
 import cs102a.aeroplane.GameInfo;
-import frontend.CustomerChoice;
 
 import java.util.ArrayList;
 
 public class Player {
     private static int cheatNum;
 
+    public static int getAns() {
+        return ans;
+    }
+
+    public static void setAns(int ans) {
+        Player.ans = ans;
+    }
+
+    private static int ans;
     public static int getCheatNum() {
         return cheatNum;
     }
@@ -18,7 +26,6 @@ public class Player {
 
     public static int askPlayerStep(int[] rollResult) {
         if (GameInfo.isIsCheatMode()) {
-
             // TODO: 2020/12/4 传入popup中用户输入的值，原始的String即可
             // FIXME: 2020/12/4 删除调试用例
             String cheat = "";
@@ -41,29 +48,21 @@ public class Player {
         } else {
             //获取这个Arraylist
 
-            ArrayList<String> possibleChoice = new ArrayList<>();
+            ArrayList<Integer> possibleChoice = new ArrayList<>();
             if (rollResult[0] + rollResult[1] <= 12)
-                possibleChoice.add(String.format("%d + %d = %d",
-                        rollResult[0], rollResult[1], rollResult[0] + rollResult[1]));
+                possibleChoice.add( rollResult[0] + rollResult[1]);
             if (rollResult[0] - rollResult[1] > 0)
-                possibleChoice.add(String.format("%d - %d = %d",
-                        rollResult[0], rollResult[1], rollResult[0] - rollResult[1]));
+                possibleChoice.add( rollResult[0] - rollResult[1]);
             if (rollResult[1] - rollResult[0] > 0)
-                possibleChoice.add(String.format("%d - %d = %d",
-                        rollResult[1], rollResult[0], rollResult[1] - rollResult[0]));
+                possibleChoice.add(rollResult[1] - rollResult[0]);
             if (rollResult[0] * rollResult[1] <= 12)
-                possibleChoice.add(String.format("%d * %d = %d",
-                        rollResult[0], rollResult[1], rollResult[1] * rollResult[0]));
+                possibleChoice.add(rollResult[1] * rollResult[0]);
             if ((rollResult[0] / (float) rollResult[1]) % 1f == 0f)
-                possibleChoice.add(String.format("%d / %d = %d",
-                        rollResult[0], rollResult[1], rollResult[0] / rollResult[1]));
+                possibleChoice.add( rollResult[0] / rollResult[1]);
             if ((rollResult[1] / (float) rollResult[0]) % 1f == 0f)
-                possibleChoice.add(String.format("%d / %d = %d",
-                        rollResult[1], rollResult[0], rollResult[1] / rollResult[0]));
+                possibleChoice.add( rollResult[1] / rollResult[0]);
             // TODO: 2020/12/4 把这个list传给前端，选择一个并把值传回来
             // 前端对应选项传回的int
-            CustomerChoice.CreateButton(possibleChoice);
-            int ans = 0;
             return ans;
         }
     }
--- Proj/src/frontend/CustomerChoice.java
+++ Proj/src/frontend/CustomerChoice.java
@@ -1,8 +1,6 @@
 package frontend;
 
-
 import cs102a.aeroplane.GameInfo;
-import cs102a.aeroplane.util.Dice;
 import cs102a.aeroplane.util.Player;
 
 import javax.swing.*;
@@ -16,78 +14,91 @@ import java.util.ArrayList;
 public class CustomerChoice {
     public static void main(String[] args) {
 
-                //没有骰子，直接显示下拉菜单
-        if(GameInfo.isIsCheatMode()){
-JComboBox<Integer>Choices=new JComboBox<Integer>();
+        //没有骰子，直接显示下拉菜单
+        if (GameInfo.isIsCheatMode()) {
+            JComboBox<Integer> Choices = new JComboBox<Integer>();
 //加入所有选项
-            for (int i = 1; i <= 12;i++){
+            for (int i = 1; i <= 12; i++) {
                 Choices.addItem(i);
             }
+            //向后台传递玩家选项
             Choices.addItemListener(new ItemListener() {
                 public void itemStateChanged(ItemEvent e) {
-                    switch (e.getStateChange()){
-                           case 1 :GiveBackAnInt(1);
-                               break;
-                           case 2 :GiveBackAnInt(2);
-                               break;
-                           case 3 :GiveBackAnInt(3);
-                               break;
-                               case 4 :GiveBackAnInt(4);
-                                   break;
-                                   case 5 :GiveBackAnInt(5);
-                                       break;
-                                       case 6 :GiveBackAnInt(6);
-                                           break;
-                                           case 7 :GiveBackAnInt(7);
-                                               break;
-                                               case 8 :GiveBackAnInt(8);
-                                                   break;
-                                                   case 9 :GiveBackAnInt(9);
-                                                       break;
-                                                       case 10 :GiveBackAnInt(10);
-                                                           break;
-                                                           case 11 :GiveBackAnInt(11);
-                                                               break;
-                                                               case 12 :GiveBackAnInt(12);
-                                                                   break;
+                    switch (e.getStateChange()) {
+                        case 1:
+                            Player.setCheatNum(1);
+                            break;
+                        case 2:
+                            Player.setCheatNum(2);
+                            break;
+                        case 3:
+                            Player.setCheatNum(3);
+                            break;
+                        case 4:
+                            Player.setCheatNum(4);
+                            break;
+                        case 5:
+                            Player.setCheatNum(5);
+                            break;
+                        case 6:
+                            Player.setCheatNum(6);
+                            break;
+                        case 7:
+                            Player.setCheatNum(7);
+                            break;
+                        case 8:
+                            Player.setCheatNum(8);
+                            break;
+                        case 9:
+                            Player.setCheatNum(9);
+                            break;
+                        case 10:
+                            Player.setCheatNum(10);
+                            break;
+                        case 11:
+                            Player.setCheatNum(11);
+                            break;
+                        case 12:
+                            Player.setCheatNum(12);
+                            break;
                     }
                 }
             });
         }
         //显示骰子，显示下拉菜单让用户选择
         else {
-JButton 确定JButton=new JButton("确定");
-            确定JButton.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
 
-                }
-            });
-JFrame frame = new JFrame("选择前进步数");
-JPanel jPanel = new JPanel();
-jPanel.setLayout( new GridLayout(4,1));
-jPanel.setPreferredSize(new Dimension(150,150));
-frame.add(jPanel, BorderLayout.CENTER);
-frame.setSize(400,400);
-frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-frame.setVisible(true);
         }
+        //向后端回传一个Integer
     }
-//创建JButtons选项
-    public static void CreateButton(ArrayList<String>list){
-        for(int i=0;i <list.size();i++){
-            JButton[]jButtons=new JButton[list.size()];
-            jButtons[i].setText(list.get(i));
-            jButtons[i].addActionListener(new ActionListener(){
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    //像后端回传选中的数字
-                GiveBackAnInt(1);
+    public static void CreateCustomerChoice(ArrayList<Integer>intarray){
+        JFrame frame = new JFrame("选择前进步数");
+        JPanel jPanel = new JPanel();
+        JButton 确定JButton=new JButton("确定");
+        确定JButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+        jPanel.add(确定JButton);
+        //向后台传递玩家选项
+        JComboBox<Integer>Choice=new JComboBox<Integer>();
+        //加入所有选项
+        for (int i = 0; i < intarray.size();i++) {
+            Choice.addItem(intarray.get(i));
+            int finalI = i;
+            Choice.addItemListener(new ItemListener() {
+                public void itemStateChanged(ItemEvent e) {
+                    Player.setAns(intarray.get(finalI));
                 }
             });
         }
+        jPanel.add(Choice);
+        jPanel.setLayout( new GridLayout(2,1));
+        jPanel.setPreferredSize(new Dimension(150,150));
+        frame.add(jPanel);
+        frame.setSize(400,400);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
-    //向后端回传一个Integer
-    public static void GiveBackAnInt(int i){
-
-    }
- }
+}
