f6af26e08cf8d87635c48466f719ef0c5d037ca0
Tonystarkw12 <72902109+Tonystarkw12@users.noreply.github.com>
2020-12-11 23:53:40 +0800

    骰子的弹窗写好了

--- Proj/src/cs102a/aeroplane/util/Player.java
+++ Proj/src/cs102a/aeroplane/util/Player.java
@@ -7,78 +7,89 @@ import java.util.ArrayList;
 
 public class Player {
     private static int cheatNum;
+    private static  ArrayList<Integer> possibleChoice;
+    private static int ans;
+    private static int[]rollresult;
+    public static int[] getRollresult() {
+        return rollresult;
+    }
+
+    public static void setRollresult(int[] rollresult) {
+        Player.rollresult = rollresult;
+    }
+
+
 
     public static ArrayList<Integer> getPossibleChoice() {
         return possibleChoice;
     }
 
-//    public static void setPossibleChoice(ArrayList<Integer> possibleChoice) {
-//        Player.possibleChoice = possibleChoice;
-//    }
+    public static void setPossibleChoice(ArrayList<Integer> possibleChoice) {
+        Player.possibleChoice = possibleChoice;
+    }
 
-    private static ArrayList<Integer> possibleChoice;
 
-//    public static int getAns() {
-//        return ans;
-//    }
+    public static int getAns() {
+        return ans;
+    }
+
 
     public static void setAns(int ans) {
         Player.ans = ans;
     }
 
-    private static int ans;
 
-//    public static int getCheatNum() {
-//        return cheatNum;
-//    }
+    public static int getCheatNum() {
+        return cheatNum;
+    }
+
 
     public static void setCheatNum(int cheatNum) {
         Player.cheatNum = cheatNum;
     }
 
-    int[] rollResult = {Dice.roll(), Dice.roll()};
 
     public static int askPlayerStep(int[] rollResult) {
+        possibleChoice = new ArrayList<>();
+        rollresult = rollResult;
         if (GameInfo.isIsCheatMode()) {
-            CustomerChoice.CreateCustomerChoice(null);
-            return cheatNum;
-//            // TODO: 2020/12/4 传入popup中用户输入的值，原始的String即可
-//            // FIXME: 2020/12/4 删除调试用例
-//            String cheat = "";
-//            while (true) {
-//                boolean hasError = false;
-//                try {
-//                    cheatNum = Integer.parseInt(cheat);
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                    hasError = true;
-//                    continue;
-//                    // TODO: 2020/12/4 JPopupMenu 应该捕获这个异常并且提示用户重新输入
-//                }
-//                if (cheatNum >= 1 && cheatNum <= 12) return cheatNum;
-//                else {
-//                    hasError = true;
-//                    // TODO: 2020/12/4 JPopupMenu 应该捕获这个异常并且提示用户重新输入
-//                }
-//            }
+            // TODO: 2020/12/4 传入popup中用户输入的值，原始的String即可
+            // FIXME: 2020/12/4 删除调试用例
+            String cheat = "";
+            while (true) {
+                boolean hasError = false;
+                try {
+                    cheatNum = Integer.parseInt(cheat);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    hasError = true;
+                    continue;
+                    // TODO: 2020/12/4 JPopupMenu 应该捕获这个异常并且提示用户重新输入
+                }
+                if (cheatNum >= 1 && cheatNum <= 12) return cheatNum;
+                else {
+                    hasError = true;
+                    // TODO: 2020/12/4 JPopupMenu 应该捕获这个异常并且提示用户重新输入
+                }
+            }
         } else {
-        //获取这个Arraylist
-        if (rollResult[0] + rollResult[1] <= 12)
-            possibleChoice.add(rollResult[0] + rollResult[1]);
-        if (rollResult[0] - rollResult[1] > 0)
-            possibleChoice.add(rollResult[0] - rollResult[1]);
-        if (rollResult[1] - rollResult[0] > 0)
-            possibleChoice.add(rollResult[1] - rollResult[0]);
-        if (rollResult[0] * rollResult[1] <= 12)
-            possibleChoice.add(rollResult[1] * rollResult[0]);
-        if ((rollResult[0] / (float) rollResult[1]) % 1f == 0f)
-            possibleChoice.add(rollResult[0] / rollResult[1]);
-        if ((rollResult[1] / (float) rollResult[0]) % 1f == 0f)
-            possibleChoice.add(rollResult[1] / rollResult[0]);
-        // TODO: 2020/12/4 把这个list传给前端，选择一个并把值传回来
-        // 前端对应选项传回的int
-        CustomerChoice.CreateCustomerChoice(possibleChoice);
-        return ans;
+            //获取这个Arraylist
+            if (rollResult[0] + rollResult[1] <= 12)
+                possibleChoice.add( rollResult[0] + rollResult[1]);
+            if (rollResult[0] - rollResult[1] > 0)
+                possibleChoice.add( rollResult[0] - rollResult[1]);
+            if (rollResult[1] - rollResult[0] > 0)
+                possibleChoice.add(rollResult[1] - rollResult[0]);
+            if (rollResult[0] * rollResult[1] <= 12)
+                possibleChoice.add(rollResult[1] * rollResult[0]);
+            if ((rollResult[0] / (float) rollResult[1]) % 1f == 0f)
+                possibleChoice.add( rollResult[0] / rollResult[1]);
+            if ((rollResult[1] / (float) rollResult[0]) % 1f == 0f)
+                possibleChoice.add( rollResult[1] / rollResult[0]);
+            // TODO: 2020/12/4 把这个list传给前端，选择一个并把值传回来
+            // 前端对应选项传回的int
+            //CustomerChoice.CreateCustomerChoice(possibleChoice);
+            return ans;
         }
     }
    /* public int[] getrollResult(int[] rollResult){
--- Proj/src/frontend/CustomerChoice.java
+++ Proj/src/frontend/CustomerChoice.java
@@ -4,6 +4,8 @@ import cs102a.aeroplane.GameInfo;
 import cs102a.aeroplane.util.Dice;
 import cs102a.aeroplane.util.Player;
 
+
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -13,13 +15,15 @@ import java.awt.event.ItemListener;
 import java.util.ArrayList;
 
 public class CustomerChoice {
+
     public static void main(String[] args) {
 
         //没有骰子，直接显示下拉菜单
-        if (GameInfo.isIsCheatMode()) {
+        if (GameInfo.isIsCheatMode() || true) {
             JFrame frame = new JFrame("选择前进步数");
+            JPanel background=new BackgroundPanel((new ImageIcon("src\\骰子背景图.jpg").getImage()));
             JPanel panel = new JPanel();
-            JButton 确定JButton = new JButton("确定");
+            JButton 确定JButton=new JButton("确定");
             确定JButton.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
                     frame.dispose();
@@ -34,76 +38,81 @@ public class CustomerChoice {
             //向后台传递玩家选项
             Choices.addItemListener(new ItemListener() {
                 public void itemStateChanged(ItemEvent e) {
-
-                    Player.setCheatNum(e.getStateChange());
-//                    switch (e.getStateChange()) {
-//                        case 1:
-//                            Player.setCheatNum(1);
-//                            break;
-//                        case 2:
-//                            Player.setCheatNum(2);
-//                            break;
-//                        case 3:
-//                            Player.setCheatNum(3);
-//                            break;
-//                        case 4:
-//                            Player.setCheatNum(4);
-//                            break;
-//                        case 5:
-//                            Player.setCheatNum(5);
-//                            break;
-//                        case 6:
-//                            Player.setCheatNum(6);
-//                            break;
-//                        case 7:
-//                            Player.setCheatNum(7);
-//                            break;
-//                        case 8:
-//                            Player.setCheatNum(8);
-//                            break;
-//                        case 9:
-//                            Player.setCheatNum(9);
-//                            break;
-//                        case 10:
-//                            Player.setCheatNum(10);
-//                            break;
-//                        case 11:
-//                            Player.setCheatNum(11);
-//                            break;
-//                        case 12:
-//                            Player.setCheatNum(12);
-//                            break;
-//                    }
+                    switch (e.getStateChange()) {
+                        case 1:
+                            Player.setCheatNum(1);
+                            break;
+                        case 2:
+                            Player.setCheatNum(2);
+                            break;
+                        case 3:
+                            Player.setCheatNum(3);
+                            break;
+                        case 4:
+                            Player.setCheatNum(4);
+                            break;
+                        case 5:
+                            Player.setCheatNum(5);
+                            break;
+                        case 6:
+                            Player.setCheatNum(6);
+                            break;
+                        case 7:
+                            Player.setCheatNum(7);
+                            break;
+                        case 8:
+                            Player.setCheatNum(8);
+                            break;
+                        case 9:
+                            Player.setCheatNum(9);
+                            break;
+                        case 10:
+                            Player.setCheatNum(10);
+                            break;
+                        case 11:
+                            Player.setCheatNum(11);
+                            break;
+                        case 12:
+                            Player.setCheatNum(12);
+                            break;
+                    }
                 }
             });
             panel.add(Choices);
-            panel.setLayout(new GridLayout(2, 1, 10, 50));
-            panel.setPreferredSize(new Dimension(150, 150));
-            frame.add(panel);
-            frame.setSize(400, 400);
+            panel.setLayout( new GridLayout(2,1,10,50));
+            panel.setPreferredSize(new Dimension(150,150));
+            background.add(panel);
+            panel.setOpaque(false);
+            frame.add(background);
+            frame.setSize(400,400);
             frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
             frame.setVisible(true);
         }
         //显示骰子，显示下拉菜单让用户选择
         else {
-            CreateCustomerChoice(Player.getPossibleChoice());
+            //这个地方传给后端Player里创建窗口
+         /*   ArrayList<Integer>intarray=new ArrayList<Integer>();
+            intarray.add(Dice.roll());
+            intarray.add(Dice.roll());
+            CreateCustomerChoice(intarray);*/
         }
         //向后端回传一个Integer
     }
-
-    public static void CreateCustomerChoice(ArrayList<Integer> intarray) {
+    public static void CreateCustomerChoice(ArrayList<Integer>intarray){
         JFrame frame = new JFrame("选择前进步数");
+        JPanel background=new BackgroundPanel((new ImageIcon("src\\骰子背景图.jpg").getImage()));
+        background.setLayout(new FlowLayout());
         JPanel jPanel = new JPanel();
-        JButton 确定JButton = new JButton("确定");
+        JButton 确定JButton=new JButton("确定");
         确定JButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
             }
         });
         //向后台传递玩家选项
-        JComboBox<Integer> Choice = new JComboBox<Integer>();
+        JComboBox<Integer>Choice=new JComboBox<Integer>();
         //加入所有选项
-        for (int i = 0; i < intarray.size(); i++) {
+        for (int i = 0; i < intarray.size();i++) {
             Choice.addItem(intarray.get(i));
             int finalI = i;
             Choice.addItemListener(new ItemListener() {
@@ -112,15 +121,23 @@ public class CustomerChoice {
                 }
             });
         }
-        JLabel label1 = new JLabel(Matchpicture.chooseImage(Dice.roll()));
-        JLabel label2 = new JLabel(Matchpicture.chooseImage(Dice.roll()));
-        jPanel.add(label1, label2);
-        jPanel.add(确定JButton);
-        jPanel.add(Choice);
-        jPanel.setLayout(new GridLayout(3, 1, 10, 50));
-        jPanel.setPreferredSize(new Dimension(150, 150));
-        frame.add(jPanel);
-        frame.setSize(400, 400);
+        JLabel label1 = new JLabel(Matchpicture.chooseImage(Player.getRollresult()[0]));
+        JLabel label2 = new JLabel(Matchpicture.chooseImage(Player.getRollresult()[1]));
+        jPanel.add(label1);
+        jPanel.add(label2);
+        JPanel Buttonpanel=new JPanel();
+        Buttonpanel.add(确定JButton);
+        Buttonpanel.add(Choice);
+        Buttonpanel.setLayout(new GridLayout(1,2,10,10));
+        Buttonpanel.setPreferredSize(new Dimension(275,45));
+        jPanel.setLayout( new GridLayout(1,2,10,10));
+        jPanel.setPreferredSize(new Dimension(400,200));
+        background.add(jPanel);
+        background.add(Buttonpanel);
+        Buttonpanel.setOpaque(false);
+        jPanel.setOpaque(false);
+        frame.add(background);
+        frame.setSize(400,400);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
